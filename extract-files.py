a9ae01a508ee0fc01273241b9a0953c638022e88
Cosmin Tanislav <demonsingur@gmail.com>
2024-09-27 21:09:56 +0300

    zeekr: add extract-files.py
    Change-Id: I586069c43a1948017ccc83f96364497ac3cdd34c

---
+++ extract-files.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import lib_fixups
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/motorola/sm8475-common',
+    'hardware/qcom-caf/sm8450',
+    'vendor/qcom/opensource/commonsys-intf/display',
+]
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/lib64/libcamximageformatutils.so': blob_fixup().replace_needed(
+        'vendor.qti.hardware.display.config-V2-ndk_platform.so',
+        'vendor.qti.hardware.display.config-V2-ndk.so',
+    ),
+    'vendor/lib64/nfc_nci.nqx.default.hw.so': blob_fixup().replace_needed(
+        'libbase.so',
+        'libbase-v33.so',
+    ),
+}
+
+module = ExtractUtilsModule(
+    'zeekr',
+    'motorola',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    add_generated_carriersettings=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'sm8475-common', module.vendor)
+    utils.run()
