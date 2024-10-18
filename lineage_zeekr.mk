06dc8fa479aa6eda625a6221dab3ef203aeb393d
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    zeekr: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_zeekr.mk
+++ lineage_zeekr.mk
@@ -35,6 +35,6 @@ PRODUCT_MODEL := motorola razr 40 ultra
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_PRODUCT=zeekr_g
-
-BUILD_FINGERPRINT := motorola/zeekr_g/msi:14/U3TZ34M.2-75-5/ee4b9d:user/release-keys
+    DeviceProduct=zeekr_g \
+    BuildDesc="zeekr_g-user 14 U3TZ34M.2-75-5 ee4b9d release-keys" \
+    BuildFingerprint=motorola/zeekr_g/msi:14/U3TZ34M.2-75-5/ee4b9d:user/release-keys
