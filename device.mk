97dbbfc2c0d1a96e6601063d58bacc28b1ff9b31
AmeChanRain <downloadbot007@gmail.com>
2025-06-08 19:14:53 +0800

    zeekr: Add Aperture configurations
    Change-Id: Ifee3deaf931a8e639e04fb10e1a1d4fa0f3d6bb6

--- device.mk
+++ device.mk
@@ -37,6 +37,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    DeviceApertureRes \
     FrameworksResDevice \
     LineageSystemUIDevice \
     SystemUIResDevice
