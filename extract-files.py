f1dbe1699c82613c5efc1987916b3518e1a3a3d3
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 13:30:36 +0200

    zeekr: Patch some blobs to depend on libtinyxml2-v34.so
    libtinyxml2 10.1.0 update which landed in BP4A contains ABI
    incompatible changes which cause runtime crashes.
    
    Change-Id: Icc67da5f754b0bb30dce6192b1e520fdf1349ac5

--- extract-files.py
+++ extract-files.py
@@ -30,6 +30,16 @@ blob_fixups: blob_fixups_user_type = {
         'libbase.so',
         'libbase-v33.so',
     ),
+    (
+        'vendor/lib64/camx.provider@2.4-impl.so',
+        'vendor/lib64/camx.provider@2.5-legacy.so',
+        'vendor/lib64/camx.provider@2.6-legacy.so',
+        'vendor/lib64/camx.provider@2.7-legacy.so',
+        'vendor/lib64/camx.device@3.5-ext-impl.so',
+        'vendor/lib64/camx.device@3.6-ext-impl.so',
+    ): blob_fixup().replace_needed(
+        'libtinyxml2.so', 'libtinyxml2-v34.so'
+    ),
 }
 
 module = ExtractUtilsModule(
